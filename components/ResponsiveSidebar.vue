8eea0889a52eea203fc4f25192a0d29a8c080a5d
falaeco <nic.prudhomme3@gmail.com>
2021-04-23 14:52:38 -0400

    Content locked to container breakpoints, sidebar scale more reliably

--- components/ResponsiveSidebar.vue
+++ components/ResponsiveSidebar.vue
@@ -1,7 +1,7 @@
 <template>
-  <div id="sidebar" class="responsive-sidebar-wrapper mborder-right">
-    <div class="responsive-sidebar text-center">
-      <ul class="content-link project-links uppercase text-xl">
+  <div id="sidebar" class="flex-grow responsive-sidebar-wrapper mborder-right">
+    <div class="text-center responsive-sidebar">
+      <ul class="text-xl uppercase content-link project-links">
         <li v-for="project in projects" :key="project.link" class="responsive-sidebar--link">
           <nuxt-link :to="`/projects/${project.link}`">
             {{ project.title }}
@@ -37,7 +37,7 @@ export default {
     top: 0;
     left: 0;
     height: 100%;
-    width: 50%;
+    min-width: 250px;
     display: none;
     z-index: 20;
   }
@@ -71,30 +71,12 @@ export default {
     border-bottom: 2px solid white;
   }
 
-  /* remove padding on narrow screen */
-  @media screen and (max-height: 500px){
-    .responsive-sidebar{
-      height: auto;
-    }
-
-    .responsive-sidebar--link{
-      @apply py-1;
-    }
-  }
-
-  @media screen and (min-width: 640px){
-    .responsive-sidebar-wrapper {
-      width: 35%;
-    }
-  }
-
   @media screen and (min-width: 1024px){
     .responsive-sidebar-wrapper {
       display: block !important; /* Prevent it from hiding if it was turned hidden in mobile view*/
       position: relative;
       height: auto;
       align-self: stretch;
-      width: 25%; /* Check content width */
     }
   }
 </style>
